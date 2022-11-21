6c555e2351a022d05b13430ce3cc1928d668959e
Guilherme Maron <guimaron@gmail.com>
2022-11-21 21:05:39 +0100

    Adding a script to generate .env file

--- worker/build-env.sh
+++ worker/build-env.sh
@@ -1,8 +1,11 @@
-FILE="./frontend/.env"
+FILE="./worker/.env"
 
 cat <<EOM >$FILE
 REACT_APP_REDIS_HOST=$REACT_APP_REDIS_HOST
 REACT_APP_REDIS_PORT=$REACT_APP_REDIS_PORT
 EOM
+
+touch FILE
+cat FILE
 
 exit 0
