ed40c00409d35349c1d34932ff759b18c452c14a
Guilherme Maron <guimaron@gmail.com>
2022-11-22 13:05:51 +0100

    Fixing ENV variable names

--- api/keys.js
+++ api/keys.js
@@ -2,11 +2,11 @@
 require('dotenv').config()
 
 module.exports = {
-    redisHost:  process.env.REACT_APP_REDIS_HOST
-,   redisPort:  process.env.REACT_APP_REDIS_PORT
-,   pgHost:     process.env.REACT_APP_PGHOST
-,   pgPort:     process.env.REACT_APP_PGPORT
-,   pgUser:     process.env.REACT_APP_PGUSER
-,   pgPassword: process.env.REACT_APP_PGPASSWORD
-,   pgDatabase: process.env.REACT_APP_PGDATABASE
+    redisHost:  process.env.REDIS_HOST
+,   redisPort:  process.env.REDIS_PORT
+,   pgHost:     process.env.PGHOST
+,   pgPort:     process.env.PGPORT
+,   pgUser:     process.env.PGUSER
+,   pgPassword: process.env.PGPASSWORD
+,   pgDatabase: process.env.PGDATABASE
 }
--- worker/keys.js
+++ worker/keys.js
@@ -2,6 +2,6 @@
 require('dotenv').config()
 
 module.exports = {
-    redisHost:  process.env.REACT_APP_REDIS_HOST
-,   redisPort:  process.env.REACT_APP_REDIS_PORT
+    redisHost:  process.env.REDIS_HOST
+,   redisPort:  process.env.REDIS_PORT
 }
